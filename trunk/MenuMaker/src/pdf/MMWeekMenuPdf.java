84d632fcef85320e8d18f4da3cedf82171f6a74e
saturn1.mc@gmail.com <saturn1.mc@gmail.com@0328ac7b-8445-33ea-12e1-08ea204a7c43>
2011-05-11 10:33:16 +0000

    iText libraries

---
+++ trunk/MenuMaker/src/pdf/MMWeekMenuPdf.java
@@ -0,0 +1,118 @@
+/**
+ * 
+ */
+package pdf;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Calendar;
+import java.util.Hashtable;
+
+import model.MMIngredient;
+import model.MMRecipe;
+import model.MMRecipeElement;
+import view.table.MMWeekMenuTable;
+
+import com.itextpdf.text.BaseColor;
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Font;
+import com.itextpdf.text.FontFactory;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.pdf.PdfPTable;
+import com.itextpdf.text.pdf.PdfWriter;
+
+/**
+ * @author cmaurice2
+ * 
+ */
+public class MMWeekMenuPdf extends Document {
+
+	public static final String FOLDER_PDF = "/print/";
+	public static final String PREFIX_PDF = "Menu_";
+	public static final String EXT_PDF = ".pdf";
+
+	public static final Font PARAGRAPH_TITLE_FONT = FontFactory.getFont(
+			FontFactory.HELVETICA, 18, Font.BOLD, BaseColor.DARK_GRAY);
+	public static final Font NORMAL_FONT = FontFactory.getFont(
+			FontFactory.HELVETICA, 10, Font.BOLD, BaseColor.DARK_GRAY);
+
+	public static final int SHOP_LIST_COL_COUNT = 4;
+	
+	private MMWeekMenuTable weekMenuTable;
+	private Paragraph weekMenuParagraph;
+	private Paragraph shopListParagraph;
+
+	public MMWeekMenuPdf(MMWeekMenuTable weekMenuTable) {
+		super();
+		this.weekMenuTable = weekMenuTable;
+		preparePdf();
+	}
+
+	private String getTarget() throws IOException {
+		File configDir = new File("." + FOLDER_PDF);
+
+		if (!configDir.exists()) {
+			configDir.mkdirs();
+		}
+
+		Calendar today = Calendar.getInstance();
+
+		return configDir.getPath() + File.separator + PREFIX_PDF
+				+ today.get(Calendar.MONTH) + "-"
+				+ today.get(Calendar.DAY_OF_MONTH) + "-"
+				+ today.get(Calendar.YEAR) + EXT_PDF;
+	}
+
+	private void preparePdf() {
+		// Week Menu paragraph
+		PdfPTable weekTable = new PdfPTable(weekMenuTable.getColumnCount());
+		
+		for(int row = 0; row < weekMenuTable.getRowCount(); row++){
+			for(int col = 0; col < weekMenuTable.getColumnCount(); col++){
+				weekTable.addCell(weekMenuTable.getValueAt(row, col).toString());
+			}
+		}
+		
+		weekMenuParagraph = new Paragraph("WeekMenu", PARAGRAPH_TITLE_FONT);
+		weekMenuParagraph.add(weekTable);
+		
+		// Shop list paragraph
+		PdfPTable shopTable = new PdfPTable(SHOP_LIST_COL_COUNT);
+		
+		Hashtable<MMIngredient, Float> quantities = new Hashtable<MMIngredient, Float>();
+		
+		for(MMRecipe recipe : weekMenuTable.getRecipes()){
+			for(MMRecipeElement element : recipe.getElements()){
+				
+				Float quantity = quantities.get(element.getIngredient());
+				
+				if(quantity == null){
+					quantity = new Float(0);
+					quantities.put(element.getIngredient(), quantity);
+				}
+				
+				quantity += element.getQuantity();				
+			}
+		}
+		
+		shopListParagraph = new Paragraph("Shop list", PARAGRAPH_TITLE_FONT);
+		shopListParagraph.add(shopTable);
+	}
+
+	public void writePdf() throws FileNotFoundException, DocumentException,
+			IOException {
+		PdfWriter
+				.getInstance(this, new FileOutputStream(new File(getTarget())));
+
+		this.open();
+
+		this.add(weekMenuParagraph);
+
+		this.add(shopListParagraph);
+
+		this.close();
+	}
+}
